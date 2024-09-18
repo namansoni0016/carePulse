b0eed58be5559230aea0d8a691a62a1acbea8549
Naman Soni <namantsoni@gmail.com>
2024-09-18 19:25:01 +0530

    Submit Button Component

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -2,9 +2,12 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { Button } from "@/components/ui/button"
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { Form } from "@/components/ui/form";
 import CustomFormField from "./CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { UserFormValidation } from "@/lib/validation";
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -16,21 +19,26 @@ export enum FormFieldType {
     SKELETON = 'skeleton',
 }
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-})
-
 const PatientForm = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const router = useRouter();
+    const [ isLoading, setIsLoading ] = useState(false);
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
         defaultValues: {
-            username: "",
+            name: "",
+            email: "",
+            phone: ""
         },
     })
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values)
+    async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
+        setIsLoading(true);
+        try {
+            // const userData = { name, email, phone };
+            // const user = await createUser(userData);
+            // if(user) router.push(`/patients/${user.$id}/register`);
+        } catch (error) {
+            console.log(error);
+        }
     }
     return (
         <Form {...form}>
@@ -66,7 +74,7 @@ const PatientForm = () => {
                     label="Phone number"
                     placeholder="(91) 1234567890"
                 />
-                <Button type="submit">Submit</Button>
+                <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
             </form>
         </Form>
     )
